79d533e90239d63ad2c38299a73cab2346200cda
MKhan1792 <kutubkhanmakda1792@gmail.com>
2022-10-29 17:47:23 +0530

    3

--- src/Components/Row/Row.jsx
+++ src/Components/Row/Row.jsx
@@ -1,10 +1,13 @@
 import axios from '../../axios'
 import React, { useEffect, useState } from 'react'
 import './Row.css'
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
 const baseURL= "https://image.tmdb.org/t/p/original/";
 
 const Row = ({title , fetchUrl ,isLargeRow}) => {
-  const [movies, setmovies] = useState([])
+  const [movies, setmovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   useEffect(() => {
     async function fetchData(){
@@ -14,6 +17,27 @@ const Row = ({title , fetchUrl ,isLargeRow}) => {
     }
     fetchData();
   }, [fetchUrl])
+  
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars:{
+      autoplay:1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if(trailerUrl){
+      setTrailerUrl('');
+    }
+    else{
+      movieTrailer(movie?.name || movies?.title || movies?.original_name || "").then(url => {
+        const urlParams = new URLSearchParams(new URL(url).search);
+        setTrailerUrl(urlParams.get('v'));
+      }).catch(error => console.log(error))
+    }
+  }
+
   console.log(movies)
 
   return (
@@ -26,11 +50,12 @@ const Row = ({title , fetchUrl ,isLargeRow}) => {
       <div className="row__posters">
         {movies.map((movie) => 
           ((isLargeRow && movie.poster_path) || (!isLargeRow && movie.backdrop_path)) && (
-            <img className={`row__poster ${isLargeRow && "row__posterLarge"}`} key={movie.id} src={`${baseURL}${
+            <img onClick={()=>handleClick(movie)} className={`row__poster ${isLargeRow && "row__posterLarge"}`} key={movie.id} src={`${baseURL}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
             }`} alt={movie.orignal_name}/>
           ))}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts}/>}
     </div>
     </>
   )
