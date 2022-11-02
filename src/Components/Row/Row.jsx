05cbda9d6c303e228255121ab564c84c3d037e53
MKhan1792 <kutubkhanmakda1792@gmail.com>
2022-11-02 19:00:51 +0530

    5

--- src/Components/Row/Row.jsx
+++ src/Components/Row/Row.jsx
@@ -1,13 +1,12 @@
 import axios from '../../axios'
 import React, { useEffect, useState } from 'react'
 import './Row.css'
-import YouTube from 'react-youtube';
-import movieTrailer from 'movie-trailer';
 const baseURL= "https://image.tmdb.org/t/p/original/";
+const YouTubeAPI_KEY= "AIzaSyCy69Nd4UzvRDhXdY39d-NTwTOhc8n731U";
+const Trailer = {YouTubeURL: (query)=>`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=${YouTubeAPI_KEY}`}
 
 const Row = ({title , fetchUrl ,isLargeRow}) => {
   const [movies, setmovies] = useState([]);
-  const [trailerUrl, setTrailerUrl] = useState("");
 
   useEffect(() => {
     async function fetchData(){
@@ -17,28 +16,21 @@ const Row = ({title , fetchUrl ,isLargeRow}) => {
     }
     fetchData();
   }, [fetchUrl])
-  
-  const opts = {
-    height: "390",
-    width: "100%",
-    playerVars:{
-      autoplay:1,
-    },
-  };
 
-  const handleClick = (movie) => {
-    if(trailerUrl){
-      setTrailerUrl('');
+  function MovieTrailer(movie){
+    if(!(movie?.name || movie?.title || movie?.original_name || movie?.original_title || "")){
+      return;
     }
     else{
-      movieTrailer(movie?.name || movies?.title || movies?.original_name || "").then(url => {
-        const urlParams = new URLSearchParams(new URL(url).search);
-        setTrailerUrl(urlParams.get('v'));
-      }).catch(error => console.log(error))
+      fetch(Trailer.YouTubeURL(movie?.name || movie?.title || movie?.original_name || movie?.original_title || ""))
+      .then(res => res.json())
+      .then(res => {
+        const bestResult = res.items[0];
+        const YouTubeTrailerURL = `https://www.youtube.com/watch?v=${bestResult.id.videoId}`
+        window.open(YouTubeTrailerURL);
+      })
     }
   }
-
-  console.log(movies)
 
   return (
     <>
@@ -50,12 +42,18 @@ const Row = ({title , fetchUrl ,isLargeRow}) => {
       <div className="row__posters">
         {movies.map((movie) => 
           ((isLargeRow && movie.poster_path) || (!isLargeRow && movie.backdrop_path)) && (
-            <img onClick={()=>handleClick(movie)} className={`row__poster ${isLargeRow && "row__posterLarge"}`} key={movie.id} src={`${baseURL}${
-              isLargeRow ? movie.poster_path : movie.backdrop_path
-            }`} alt={movie.orignal_name}/>
+            <>
+              <div class="square">
+                <img onClick={()=>MovieTrailer(movie)} className={`row__poster ${isLargeRow && "row__posterLarge"}`} key={movie.id} src={`${baseURL}${
+                isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie?.name || movie?.title || movie?.original_name || movie?.original_title || ""}/>
+                <span class="text">
+                  {movie?.name || movie?.title || movie?.original_name || movie?.original_title || ""}
+                </span>
+              </div>
+            </>
           ))}
       </div>
-      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts}/>}
+      
     </div>
     </>
   )
